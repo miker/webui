996c2f4aec3ae87f2c5bde6771f35980e2194b94
Damian Szidiropulosz <damianos@knoxbug.org>
2017-10-16 11:09:45 -0400

    Added rest call to populate cards with real data.

--- src/app/pages/examples/example-cards/example-cards.component.ts
+++ src/app/pages/examples/example-cards/example-cards.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { EntityModule } from '../../common/entity/entity.module';
+import { RestService } from '../../../services/';
 
 interface CardConfig {
   header: string;
   body: string;
   footer: string;
+}
+
+interface UserProfile {
+  username: string;
+  fullname: string;
+  uid: string;
+  gid: string;
+  homeDirectory: string;
+  shell: string;
 }
 
 @Component({
@@ -16,11 +26,30 @@ interface CardConfig {
 export class ExampleCardsComponent implements OnInit {
 
   @Input() cards = [];
-  constructor() {}
+  constructor(protected rest: RestService) {}
 
   ngOnInit() {
-    this.init(16);
+    this.getUserList();
+    //this.init(16);
     //this.gridCols = 1;
+  }
+
+  getUserList() {
+    this.rest.get('account/users', {}).subscribe((res) => {
+      console.log(res);
+      for(var i = 0; i < res.total; i++){
+	var card: UserProfile = {
+	  username: res.data[i].bsdusr_username,
+	  fullname: res.data[i].bsdusr_full_name,
+	  uid: res.data[i].bsdusr_uid,
+	  gid: res.data[i].bsdusr_group,
+	  homeDirectory: res.data[i].bsdusr_home,
+	  shell: res.data[i].bsdusr_shell
+	}
+	console.log(card);
+	this.cards.push(card);
+      }
+    })
   }
 
   private init(num:number){
