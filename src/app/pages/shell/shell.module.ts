60dad35adfc895fa86acda9913c5e8c2c07fae6d
Lola Yang <lola.ly27@gmail.com>
2018-01-17 17:06:47 -0800

    Fixed mat-divider and mat-slider issue on shell page

--- src/app/pages/shell/shell.module.ts
+++ src/app/pages/shell/shell.module.ts
@@ -2,14 +2,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatCommonModule, MatCardModule } from '@angular/material';
-import {EntityModule} from '../common/entity/entity.module';
+import { MatCommonModule, MatCardModule, MatListModule, MatSliderModule } from '@angular/material';
+import { EntityModule } from '../common/entity/entity.module';
 //Component Modules
 import { ShellComponent } from './shell.component';
 import { routing } from './shell.routing';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, EntityModule, routing, MatCommonModule, MatCardModule],
+  imports: [CommonModule, FormsModule, EntityModule, routing, MatCommonModule, MatCardModule, MatListModule, MatSliderModule],
   declarations: [
     ShellComponent
   ],
